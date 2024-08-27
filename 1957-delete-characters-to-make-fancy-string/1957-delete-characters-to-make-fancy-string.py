f67f28ec8789fb63844e0dcf3e958f972d7e7f31
HyoJun2 <127198895+HyoJun2@users.noreply.github.com>
2024-08-27 10:07:24 +0900

    Time: 215 ms (95.62%), Space: 18.4 MB (75.9%) - LeetHub

--- 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.py
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.py
@@ -1,8 +1,36 @@
 class Solution:
     def makeFancyString(self, s: str) -> str:
+        # answer = []
+        # for i in s:
+        #     if len(answer) > 1 and i == answer[-1] == answer[-2]:
+        #         answer.pop()
+        #     answer.append(i)
+        # return ''.join(answer)
+
+        # answer = ""
+        # prev = ""
+        # cnt = 0
+        # for c in s:
+        #     if c == prev:
+        #         cnt += 1:
+        #     else:
+        #         cnt = 1:
+        #     if cnt < 3:
+        #         answer.append(c)
+        #     return answer
+
         answer = []
-        for i in s:
-            if len(answer) > 1 and i == answer[-1] == answer[-2]:
-                answer.pop()
-            answer.append(i)
+        prev = ""
+        cnt = 0
+        for c in s:
+            if c == prev:
+                cnt += 1
+            else:
+                cnt = 1
+            
+            if cnt < 3:
+                answer.append(c)
+            
+            prev = c
+        
         return ''.join(answer)
