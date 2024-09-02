2dc1b0e494cce2200269488fbc0f27760f187617
HyoJun2 <127198895+HyoJun2@users.noreply.github.com>
2024-09-02 09:45:27 +0900

    Time: 28 ms (95.53%), Space: 16.4 MB (91.44%) - LeetHub

--- 3168-minimum-number-of-chairs-in-a-waiting-room/3168-minimum-number-of-chairs-in-a-waiting-room.py
+++ 3168-minimum-number-of-chairs-in-a-waiting-room/3168-minimum-number-of-chairs-in-a-waiting-room.py
@@ -1,12 +1,23 @@
 class Solution:
     def minimumChairs(self, s: str) -> int:
-        max_chairs = 0
-        current_chairs = 0
+        # max_chairs = 0
+        # current_chairs = 0
 
-        for i in s:
-            if i == "E":
-                current_chairs += 1
-                max_chairs = max(max_chairs, current_chairs)
+        # for i in s:
+        #     if i == "E":
+        #         current_chairs += 1
+        #         max_chairs = max(max_chairs, current_chairs)
+        #     else:
+        #         current_chairs -= 1
+        # return max_chairs
+
+        answer = 0
+        cnt = 0
+        for c in s:
+            if c == "E":
+                cnt += 1
+                answer = max(answer, cnt)
             else:
-                current_chairs -= 1
-        return max_chairs
+                cnt -= 1
+            # 방안에 머무는 사람의 최대 수를 answer로 지정
+        return answer
