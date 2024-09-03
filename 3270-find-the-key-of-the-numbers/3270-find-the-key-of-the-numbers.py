0bf302632634def51f4431894857fb2c87e92208
HyoJun2 <127198895+HyoJun2@users.noreply.github.com>
2024-09-03 10:04:59 +0900

    Time: 46 ms (9.2%), Space: 16.6 MB (48.82%) - LeetHub

--- 3270-find-the-key-of-the-numbers/3270-find-the-key-of-the-numbers.py
+++ 3270-find-the-key-of-the-numbers/3270-find-the-key-of-the-numbers.py
@@ -6,6 +6,10 @@ class Solution:
         str_3 = str(num3).zfill(4)
         
         answer = []
-        for i in range(4):
-            answer.append(min(str_1[i], str_2[i], str_3[i]))
+        # for i in range(4):
+        #     answer.append(min(str_1[i], str_2[i], str_3[i]))
+        # return int("".join(answer))
+
+        for chr_1, chr_2, chr_3 in zip(str_1, str_2, str_3):
+            answer.append(min(chr_1, chr_2, chr_3))
         return int("".join(answer))
